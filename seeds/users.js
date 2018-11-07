ad4cbbb5ff3385aef87c989de349ab7c4472b1a6
Jeremy-Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-08 11:40:32 +1300

    fixed linting errors

--- seeds/users.js
+++ seeds/users.js
@@ -1,8 +1,10 @@
 exports.seed = function (knex, Promise) {
   return knex('users').del()
     .then(function () {
-      return knex('users').insert([
-        {id: 99901, name: 'Ambitious Aardvark', email: 'aardvark@example.org'}
-      ])
+      return knex('users').insert([{
+        id: 99901,
+        name: 'Ambitious Aardvark',
+        email: 'aardvark@example.org'
+      }])
     })
 }
